0be9218a9722a137973a122b2ba18d56009d0d30
bobbyhadz <opmftw@gmail.com>
2024-04-05 11:05:43 +0300

    minor changes

---
+++ index.js
@@ -0,0 +1,24 @@
+// Replace a String in a File using Node.js
+
+import {
+  readFile,
+  writeFile,
+  writeFileSync,
+  promises as fsPromises,
+} from 'fs';
+
+readFile('./example.txt', 'utf-8', function (err, contents) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const replaced = contents.replace(
+    /to be replaced/g,
+    'replacement string',
+  );
+
+  writeFile('./example.txt', replaced, 'utf-8', function (err) {
+    console.log(err);
+  });
+});
